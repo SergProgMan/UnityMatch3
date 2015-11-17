f01cc456e263c96025e0fd7e8df88f28a6bb5ec1
SergProgMan <semibratyev@GMAIL.COM>
2015-11-17 22:59:43 +0200

    Update Sphere.cs
    added some changes

--- Assets/MyMatch3/scripts/Sphere.cs
+++ Assets/MyMatch3/scripts/Sphere.cs
@@ -7,128 +7,96 @@ public class Sphere : MonoBehaviour {
 	public int x;
 	public int y;
 
-	public int dY;
+	public int moveDown;
 	public int scoreValue;
 
 	public bool matched;
-	public bool destroyed;
-
+	
 	public bool readyToMove;
 	public bool fallEfect;
 
 	private Vector3 myScale;
-//	private float startTime;
 
 	public static Transform select;
 	public static Transform moveTo;
-
-
-
-
-	void Start()
-	{
+	
+	void Start(){
 		myScale = transform.localScale;
-		//startTime = Time.time;
-		if (fallEfect)
-		{
+		
+		if (fallEfect){
 			StartCoroutine ("FallDown");
 			fallEfect = false;
-
-
 		}
+	}
+
+	void Update (){
 
 	}
 
-	void Update ()
-	{
-
-	}
-
-	void OnMouseOver()
-	{
+	void OnMouseOver(){
 		transform.localScale = new Vector3(myScale.x+0.2f, myScale.y+0.2f,myScale.z+0.2f);
-		if (Input.GetMouseButtonDown(0))
-		{
-			if(!select)
-			{
+		if (Input.GetMouseButtonDown(0)){
+			if(!select){
 				select = transform;
 				transform.localScale = new Vector3(myScale.x+0.2f, myScale.y+0.2f,myScale.z+0.2f);
 			}
-			else if(select != transform && !moveTo)
-			{
+			else if(select != transform && !moveTo)	{
 				moveTo = transform;
 			}
 		}
 	}
 
-	void OnMouseExit ()
-	{
+	void OnMouseExit (){
 		transform.localScale = myScale;
 	}
 
-	public IEnumerator MoveDown (int downY)
-	{
+	public IEnumerator MoveDown (int downY){
 		Vector3 correctPosition = transform.position;
 		Vector3 newPosition = new Vector3 (transform.position.x, transform.position.y - downY, transform.position.z);
+	
 		float time  = 0;
 
-		while(time<1)
-		{
+		while(time<1){
 			time += Time.deltaTime*4;
 			transform.position = Vector3.Lerp (correctPosition, newPosition, time);
 			yield return null;
 		}
 
 		readyToMove =false;
-		CheckPlace();
 	}
 
-	public IEnumerator FallDown ()
-	{
-	
-
-
+	public IEnumerator FallDown (){
 		Vector3 newPosition = transform.position;
 		Vector3 correctPosition = new Vector3 (transform.position.x, transform.position.y+dY, transform.position.z);
 		float time = 0;
 
-		while (time<1)
-		{
+		while (time<1){
 			time += Time.fixedDeltaTime*4;
 			transform.position= Vector3.Lerp (correctPosition, newPosition, time);
 			yield return null;
 		}
-		//Grid.CheckMatchAfterSpawn();
-
-
 	}
 
-	public IEnumerator DestroyBlock()
-	{
+	public IEnumerator DestroyBlock(){
 		
 		Vector3 correctPosition = transform.localScale;
 
 		float time = 0;
 
-		while (time<1)
-		{
+		while (time<1){
 			time += Time.deltaTime*4;
 			transform.localScale = Vector3.Lerp (correctPosition, Vector3.zero, time);
 			yield return null;
 		}
-
+		
 		ScoreManager.score += scoreValue;
-		Grid.canCheckMatch =true;
 		Destroy (gameObject);
-
-
 	}
-	public void CheckPlace()
-	{
+	
+	public void CheckPlace(){
 		Vector3 dPos = transform.position;
 		Vector3 cPos= new Vector3(x,y,0);
 		transform.position = Vector3.Lerp (dPos,cPos,1);
-		//Grid.CheckMatchAfterSpawn();
 	}
-	}
+}
 
